9f52cbfdb83dfe639d04f0786dddff6dc22c3fac
Ali Aliyev <tu4pw6wq5@aztu.local>
2022-03-05 14:11:42 +0400

    added task

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            int[] array = { 5, 2, 3, 10, -2 };
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+
+            }
+            Console.WriteLine(min);
+            #endregion
+
+
+        }
+    }
+}
